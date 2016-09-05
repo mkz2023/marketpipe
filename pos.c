8e94834cda69888531799c7e822d2d6fd1989a06
Quintero Rubio Martin <mquinteror@mquinterordebian.www.isb.pdmu.mx>
2016-09-05 12:46:28 -0500

    trabajo en la funcion principal

--- pos.c
+++ pos.c
@@ -4,5 +4,7 @@
 /* Sistema de punto de venta para usar en sistemas  like-UNIX en cualquier distribucion, desarrollado por probecarios de proteco de la generacion 33 */
 
 int main ( int argc, char const *argv[] ){
-  
+  printf("Estamos iniciando este proyecto\n");
+
+  return 0;
 }
