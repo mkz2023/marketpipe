28ef940c057612b514d8a21da7f8ef21924c703c
mquinteror <martinsilloelfilosofo@gmail.com>
2016-09-11 21:49:20 -0500

    ''

--- gitea.sh
+++ gitea.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
-#como publicar los cambios en git esta medio aburrido, mejor que lo haga bash por nosotros
+echo "el commit llevara el siguiente comentario"
+echo $1
 git add *
-git commit -m $1
+git commit -m \'$1\'
 git remote add origin git@github.com:mquinteror/pos.git
 git push -u origin master
 git status
