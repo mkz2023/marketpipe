3bd722acee8008a57e14e2bec6bd3ece868fe0f4
mquinteror <martinsilloelfilosofo@gmail.com>
2016-09-11 22:01:28 -0500

    sigue sin funcionr el script

--- gitea.sh
+++ gitea.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-echo "el commit llevara el siguiente comentario"
-echo $1
-git add *
-git commit -m \'$1\'
+#echo "el commit llevara el siguiente comentario"
+#echo $1
+#git add *
+#git commit -m \'$1\'
 git remote add origin git@github.com:mquinteror/pos.git
 git push -u origin master
 git status
