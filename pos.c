50be8af911f9d604545d07d4fd5d2401c7cc33da
mquinteror <martinsilloelfilosofo@gmail.com>
2016-09-11 20:54:04 -0500

    agregando al repo de github

--- pos.c
+++ pos.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 
 /* Sistema de punto de venta para usar en sistemas  like-UNIX en cualquier distribucion, desarrollado por probecarios de proteco de la generacion 33 */
+
+
+
 int login ( void ){
   char usuario[10];
   int pass;
