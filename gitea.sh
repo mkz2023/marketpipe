992766c2560936415a8c4ae36debb94b1e13673b
mquinteror <martinsilloelfilosofo@gmail.com>
2016-09-11 21:12:23 -0500

    un scrpit para facilitar las cosas

---
+++ gitea.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#como publicar los cambios en git esta medio aburrido, mejor que lo haga bash por nosotros
+
+git remote add origin git@github.com:mquinteror/pos.git
+git push -u origin master
+
