2a59701ce852932141f232b307300baf7155d89d
mquinteror <martinsilloelfilosofo@gmail.com>
2016-09-11 21:35:02 -0500

    sistema de loggin

--- gitea.sh
+++ gitea.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 #como publicar los cambios en git esta medio aburrido, mejor que lo haga bash por nosotros
-
+git add *
+git commit -m $1
 git remote add origin git@github.com:mquinteror/pos.git
 git push -u origin master
-
+git status
--- pos.c
+++ pos.c
@@ -3,7 +3,27 @@
 
 /* Sistema de punto de venta para usar en sistemas  like-UNIX en cualquier distribucion, desarrollado por probecarios de proteco de la generacion 33 */
 
+void pos ( void  ){
+  printf("bienvenido al sistema de punto de venta\n");
+}
 
+int localizarUsuario ( char usuario[10] ){
+  if ( usuario == "martin" ){
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+int verificarPass ( char usuario[10], int pass ){
+  if ( usuario == "martin" && pass==1992 ){
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
 
 int login ( void ){
   char usuario[10];
@@ -13,17 +33,30 @@ int login ( void ){
   
   do {
     printf( "Usuario: " );
-    scanf("%s", &usuario);
-    printf("\Pass: ");
+    scanf("%s", usuario);
+    printf("\nPass: ");
     scanf("%d", &pass);
-    if (  )
-  } while ( intentos != 5 );
+    if ( localizarUsuario( usuario ) == 1 ){
+      if ( verificarPass( usuario, pass ) == 1 ){
+	// garantizar el accero
+	pos();
+      }
+      else {
+	printf("Verificar Pass\n");
+	intentos ++;
+      }
+    }
+    else {
+      printf("Verificar Usuario\n");
+      intentos++;
+    }
+    printf("intentos [%d]/[5]\n");
+  } while ( intentos <= 5 );
 }
 
 int main ( int argc, char const *argv[] ){
   printf("PREBEPOS\n");
-  
-  
+  login();
   return 0;
 }
 
